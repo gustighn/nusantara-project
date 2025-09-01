7e7de37fec5b19031d66bf2ce8f21b0ed96c8cb5
gustighn <gustigani903@gmail.com>
2025-09-01 21:18:58 +0800

    update

--- script.js
+++ script.js
@@ -10,18 +10,3 @@ document.addEventListener("click", function (e) {
     navMenu.classList.remove("active");
   }
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   window.addEventListener("scroll", function () {
-//     if (window.scrollY > 50) {
-//       document.getElementById("navbar").classList.add("fixed");
-//       document.getElementById("hero-container").style.marginTop = "120px";
-//       document.getElementById("hero-container").style.transition = "all 1s";
-//       navMenu.style.top = "6.5rem";
-//     } else {
-//       navMenu.style.top = "5.5rem";
-//       document.getElementById("navbar").classList.remove("fixed");
-//       document.getElementById("hero-container").style.marginTop = "0";
-//     }
-//   });
-// });
